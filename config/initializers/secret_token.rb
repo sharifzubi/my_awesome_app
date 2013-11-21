575fcd720ff5a2423830c2d631ab290f41ef9856
sharifzubi <szubi@me.com>
2013-11-21 11:55:35 +0000

    git initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyAwesomeApp::Application.config.secret_token = '9973ee714a3423577398dce868b5d1d80f1380e387c229cc17430b8a38bf4c54cd929d750ad9aae57c5f1e5a38e7b2192cbf0f09027d99251b203babef6682d8'
